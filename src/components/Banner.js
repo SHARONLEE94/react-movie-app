b4f532df3abb042a2d618792dedcfabee2afd39b
sharon <sharon329@naver.com>
2025-02-14 16:48:18 +0900

    ✨ Feat(imgBanner)
    Banner 컴포넌트 구현

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React , { useEffect, useState }from 'react'
+import axios from '../api/axios'
+import requests from '../api/request'
 
 const Banner = () => {
+  
+  const [movie, setMovie] = useState([]);
+    
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  const fetchData = async () => {
+    const response = await axios.get(requests.fetchNowPlaying);
+
+    const movieId = response.data.results[
+      Math.floor(Math.random() * response.data.results.length)
+    ].id
+
+    const { data:movieDetail } = await axios.get(`movie/${movieId}`, {
+      params: { append_to_response:"videos"}
+    })
+    setMovie(movieDetail)
+    console.log(movieDetail)
+  }
+  
   return (
     <div>
-      
+      <div>{movie.title}</div>
+      <div><br/></div>
+      <div>{movie.overview}</div>
     </div>
   )
 }
