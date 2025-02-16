eebd05965519fcc19fcd93eca211d84bea52e686
sharon <sharon329@naver.com>
2025-02-16 13:09:38 +0900

    ✨ Feat(modal) 모달 기능 추가

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect, useCallback } from 'react'
 import axios from '../api/axios'
 import './Row.css'
+import MovieModal from './MoviModal';
 
 function Row({title, id, fetchUrl }) {
 
   const [movies, setMovies] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({})
 
   const fetchMovieData = useCallback(async () => {
     const response = await axios.get(fetchUrl);
@@ -15,6 +18,11 @@ function Row({title, id, fetchUrl }) {
     fetchMovieData();
   }, [fetchMovieData])
 
+  const handleClick = (movie)=>{
+    setModalOpen(true)
+    setMovieSelected(movie)
+    console.log(movie)
+  }
 
   
   return (
@@ -33,14 +41,13 @@ function Row({title, id, fetchUrl }) {
           </span>
         </div>
         <div id={id} className="row__posters">
-          {/* {console.log(movies)} */}
           {movies.map(movie => (
             <img 
               key={movie.id}
               className='row__poster'
               src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
               alt={movie.name}
-              // onClick={() => handleClick(movie)}
+              onClick={() => handleClick(movie)}
             />
           ))}
         </div>
@@ -55,6 +62,13 @@ function Row({title, id, fetchUrl }) {
           </span>
         </div>
       </div>
+      {console.log(modalOpen)}
+      { modalOpen &&
+        <MovieModal
+          {...movieSelected}
+          setModalOpen = {setModalOpen}
+        />
+      }
     </div>
   )
 }
