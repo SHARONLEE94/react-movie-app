c8a31aee1ddf1891ba53940a580dcf99302286ab
sharon <sharon329@naver.com>
2025-02-17 21:20:27 +0900

    💄 UI/CSS : swiper 모듈 CSS
    터치슬라이드 구현

--- src/components/Row.js
+++ src/components/Row.js
@@ -13,7 +13,7 @@ import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 
-function Row({title, id, fetchUrl }) {
+function Row({title, id, fetchUrl}) {
 
   const [movies, setMovies] = useState([]);
   const [modalOpen, setModalOpen] = useState(false);
@@ -31,26 +31,42 @@ function Row({title, id, fetchUrl }) {
   const handleClick = (movie)=>{
     setModalOpen(true)
     setMovieSelected(movie)
-    console.log(movie)
   }
 
   
   return (
     <Container>
       <h2>{title}</h2>
-      <Swiper
+      <Swiper // 자세한 옵션은 swiper 홈페이지 Docs를 찾아보면 됨
         modules={[Navigation, Pagination, Scrollbar, A11y]}
         loop={true}
         navigation
         pagination={{clickable:true}}
+        breakpoints={{
+          1378: {
+            slidesPerView: 6, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 6,
+          },
+          998: {
+            slidesPerView: 5, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 5,
+          },
+          625: {
+            slidesPerView: 4, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 4,
+          },
+          0: {
+            slidesPerView: 3, //한번에 보이는 슬라이드 개수 
+            slidesPerGroup: 3,
+          },
+        }}
       >
         <Content id={id}>
           {movies.map(movie => (
-            <SwiperSlide>
+            <SwiperSlide key={movie.id}>
               <Wrap>
                 <img 
                   key={movie.id}
-                  className='row__poster'
                   src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`}
                   alt={movie.name}
                   onClick={() => handleClick(movie)}
@@ -79,4 +95,34 @@ const Container = styled.div`
 const Content = styled.div`
 
 `
-const Wrap = styled.div``
+const Wrap = styled.div`
+  width: 95%;
+  height: 95%;
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0/69%) 0px 26px 30px -10px,
+              rgb(0 0 0/73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+
+  img {
+    inset: 0px;
+    display: block;
+    height: 100%;
+    object-fit: cover;
+    opacity: 1;
+    position: absolute;
+    width: 100%;
+    transition: opacity 500ms ease-in-out;
+    z-index:1;
+  }
+  &:hover {
+    box-shadow: rgb(0 0 0 / 80%) 0px 40px 58px -16px,
+       rgb(0 0 0 / 72%) 0px 30px 22px -10px;
+    transform: scale(0.98);
+    border-color: rgba(249, 249, 249, 0.8);
+  }
+`;
